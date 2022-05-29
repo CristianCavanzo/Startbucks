0d3635674f7b4428412f5e55b0133aa92ce38c7f
Cristian Cavanzo <cristian.frontendev@gmail.com>
2022-05-29 18:48:34 -0500

    Stable Feat: -Initial config

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+    reactStrictMode: true,
+    experimental: {
+        // Enables the styled-components SWC transform
+        styledComponents: true,
+    },
+};
